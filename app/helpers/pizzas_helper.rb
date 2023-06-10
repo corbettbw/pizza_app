3584fea331733a434aa33239cbf53813714be640
Corbett Winningham <corbettwinningham@Corbetts-MacBook-Pro.local>
2023-06-10 16:55:55 -0700

    adding the option to keep expired toppings

--- app/helpers/pizzas_helper.rb
+++ app/helpers/pizzas_helper.rb
@@ -6,4 +6,18 @@ module PizzasHelper
             return true
         end
     end
+
+    def topping_expired(pizza)
+        if pizza.toppings != nil
+            toppingsArray = []
+            JSON.parse(pizza.toppings).each do |topping|
+                if Topping.all.collect(&:name).include?(topping) == false
+                    toppingsArray.push topping
+                end
+            end
+            toppingsArray.empty? ? (toppingsArray.push false):(toppingsArray.push true)
+        end
+    end
 end
+# conditions: pizza exists, has toppings, and one of the toppings doesn't exist
+# include check box and topping in red with warning: uncheck if you want to remove this topping
